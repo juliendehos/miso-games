d3758e0d9ac0ec60c13f89fce7b7f26841f6363f
Julien Dehos <dehos@univ-littoral.fr>
2025-08-25 23:02:36 +0200

    Minesweeper: fix color

--- src/Minesweeper/Component.hs
+++ src/Minesweeper/Component.hs
@@ -34,12 +34,23 @@ colorWrongMine = CSS.hex 0xDD8888
 
 n2color :: Int -> CSS.Color
 n2color = \case
-  1 -> CSS.hex 0x0000FF
+  1 -> CSS.Hex "0000FF"
+  2 -> CSS.Hex "007B00"
+  3 -> CSS.Hex "FF0000"
+  4 -> CSS.Hex "00007B"
+  5 -> CSS.Hex "7B0000"
+  _ -> CSS.black
+
+{-
+n2color :: Int -> CSS.Color
+n2color = \case
+  1 -> CSS.hex 0x0000FF   -- TODO fix bug
   2 -> CSS.hex 0x007B00
   3 -> CSS.hex 0xFF0000
   4 -> CSS.hex 0x00007B
   5 -> CSS.hex 0x7B0000
   _ -> CSS.black
+-}
 
 -------------------------------------------------------------------------------
 -- helpers
